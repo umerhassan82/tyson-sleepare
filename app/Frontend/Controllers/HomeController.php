b098e9328f7cf71f5fe6a5bc7aba4da80a6917da
Umer Hassan <umerhassan82@gmail.com>
2019-07-05 22:25:36 +0500

    db connection

--- app/Frontend/Controllers/HomeController.php
+++ app/Frontend/Controllers/HomeController.php
@@ -164,7 +164,7 @@ class HomeController extends Controller
                 else if($myProduct === "avocado")
                     $myProductUrl = 'https://shareasale.com/r.cfm?b=939087&u=1718190&m=68778&urllink=&afftrack=';
 
-                $servername = "localhost";
+                $servername = "127.0.0.1";
                 $username 	= "sleepmag_store";
                 $password 	= "(hS63x-p87";
                 $dbname 	= "sleepmag_store";
@@ -373,7 +373,7 @@ class HomeController extends Controller
                         $password = 'leH48TkyupQw';
                     }
                 }
-                $servername = "localhost";
+                $servername = "127.0.0.1";
                 $username 	= "sleepmag_store";
                 $passworddb = "(hS63x-p87";
                 $dbname 	= "sleepmag_store";
