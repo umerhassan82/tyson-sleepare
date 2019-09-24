b668931dfc0ce76bb0a565104b1f600ed920bf5d
Umer Hassan <umerhassan82@gmail.com>
2019-09-24 20:38:07 +0500

    Changed pass

--- app/Frontend/Controllers/HomeController.php
+++ app/Frontend/Controllers/HomeController.php
@@ -356,7 +356,7 @@ class HomeController extends Controller
                 $empID = 'Genero';
                 $empName = 'Genero';
                 $emailFrom = 'genaro@sleepare.com';
-                $password = 'sleepare1234%';
+                $password = 'genaro123';
                 
                 $servername = "185.56.87.173";
                 $username 	= "sleepmag_store";
